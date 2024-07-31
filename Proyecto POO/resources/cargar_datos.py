62bd85bcfecc7f61d8f2ce7bb97dc649bbafea13
BCa209 <156900278+BCa209@users.noreply.github.com>
2024-07-31 11:30:10 -0500

    Delete Proyecto POO directory

--- Proyecto POO/resources/cargar_datos.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-@st.cache_data
-def cargar_datos():
-        file_path = 'data/matricula_uni.csv'
-        return pd.read_csv(file_path, delimiter=';')
